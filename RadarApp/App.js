e8d88565cb786625594b9f39bbce930631e79103
Sabrina <wickersl@rose-hulman.edu>
2017-12-18 01:26:07 -0500

    Redid project initialization. Added a first screen. If you have an emulator and run the app on it, you should see the single screen I made.

---
+++ RadarApp/App.js
@@ -0,0 +1,65 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  Image,
+  View
+} from 'react-native';
+
+// const instructions = Platform.select({
+//   ios: 'Press Cmd+R to reload,\n' +
+//     'Cmd+D or shake for dev menu',
+//   android: 'Double tap R on your keyboard to reload,\n' +
+//     'Shake or press menu button for dev menu',
+// });
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image source={require('./app/images/rose_r_placeholder.png')}
+          style={styles.logo}/>
+        <Text>
+          Welcome to the RADAR app!
+        </Text>
+        <Text>
+          Please log in using your Kerberos username and password.
+        </Text>
+        {/* <Text style={styles.instructions}>
+          {instructions}
+        </Text> */}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  logo: {
+    width: 400,
+    height: 400
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
