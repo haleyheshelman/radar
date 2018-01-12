667959000053656d66e79253cafc592c260548d9
Vibha R Alangar <alangavr@rose-hulman.edu>
2018-01-12 16:16:17 -0500

    added testdatabase file;

--- RadarApp/App.js
+++ RadarApp/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StackNavigator} from 'react-navigation';
 
+import * as firebase from 'firebase';
 import LoginScreen from './app/screens/Login';
 import DutyLandingScreen from './app/screens/DutyLanding';
 import ViewScheduleScreen from './app/screens/ViewSchedule';
@@ -12,5 +13,18 @@ const App = StackNavigator({
   ViewSchedule: {screen: ViewScheduleScreen},
   SwapBoard: {screen: SwapBoardScreen},
 })
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB2fTVAiHoac7oSicifltldkwb8-cT-MBw",
+    authDomain: "vash-radar.firebaseapp.com",
+    databaseURL: "https://vash-radar.firebaseio.com",
+    projectId: "vash-radar",
+    storageBucket: "vash-radar.appspot.com",
+    messagingSenderId: "295841063943"
+};
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+
+
 
 export default App;
